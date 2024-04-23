c7da867bfdd3194d8793be3a87ea35fb8b6dfd76
Yevhenii <yevmot01@easv365.dk>
2024-04-23 11:52:31 +0200

    login form ready

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,19 @@
+<!-- LoginForm.vue -->
+<template>
+    <div>
+        <h1>Log In form</h1>
+        <input placeholder="Username" v-model="email" />
+        <input placeholder="Password" type="password" v-model="password" />
+        <button @click="logIn">Login</button>
+    </div>
+</template>
+
+<script setup>
+import { login } from '../modules/login.js';
+const { email, password, logIn } = login();
+
+</script>
+
+<style scoped>
+
+</style>
