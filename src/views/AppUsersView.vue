d4885ef553176d6fe786bec4cc1b1f0db6c24011
Oleh <118124646+Olehq312@users.noreply.github.com>
2024-04-26 13:26:26 +0200

    style for dropdown menu for adding users done

--- src/views/AppUsersView.vue
+++ src/views/AppUsersView.vue
@@ -1,28 +1,30 @@
 <!-- AppView.vue -->
 <template>
 
-  <body class="font-futura flex items-center flex-col w-[100%]">
-    <div class="h1text">
-      <h1 class="text-p text-main">User Info:</h1>
+  <body class="font-futura flex flex-col items-center w-[100%]">
+    <div class="h1text flex justify-start w-[70%]">
+      <h1 class="text-[25px] text-main">User Info:</h1>
     </div>
-    <div class="info flex justify-between w-[100%]">
-      <div class="emailcl">
+    <div class="info flex justify-between w-[70%] mt-3 mb-5">
+      <div class="emailcl w-[45%] ">
         <h1 class="text-text">Email</h1>
-        <input class="border rounded-lg border-text" v-model="email" />
+        <input class="border rounded-lg border-text w-[100%]" v-model="email" />
       </div>
-      <div class="passwordcl">
+      <div class="passwordcl w-[45%]">
         <h1>Password</h1>
-        <input placeholder="" type="password" v-model="password" />
+        <input class="border rounded-lg border-text w-[100%]" type="password" v-model="password" />
       </div>
     </div>
-    <div>
-      <label>
-        <input type="radio" value="user" v-model="selectedRole" /> User
-      </label>
-      <label>
-        <input type="radio" value="admin" v-model="selectedRole" /> Admin
-      </label>
-      <button @click="signUp(selectedRole)">Sign Up</button>
+    <div class="flex justify-between w-[70%]">
+      <div class=" w-[20%] flex justify-between text-h3">
+        <label>
+          <input type="radio" value="user" v-model="selectedRole" /> User
+        </label>
+        <label>
+          <input type="radio" value="admin" v-model="selectedRole" /> Admin
+        </label>
+      </div>
+      <button class=" bg-main text-white px-[60px] rounded-2xl" @click="signUp(selectedRole)">Save</button>
     </div>
   </body>
 </template>
