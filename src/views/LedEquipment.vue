86b7e6dd3940263dc3dfdd883177e10120405996
Oleh <118124646+Olehq312@users.noreply.github.com>
2024-05-10 16:53:48 +0200

    equipment page start designing

--- src/views/LedEquipment.vue
+++ src/views/LedEquipment.vue
@@ -1,13 +1,19 @@
 <template>
-    <div>
-        You are viewing the LedEquipment.vue file
 
-        <div class="">
-            <a href="/oxyequipment">OxygenEquipment</a>
-            <a href="/ledequipment">LedEquipment</a>
-            <a href="/microequipment">MicroEquipment</a>
+    <body class="font-futura">
+
+
+        <div>
+            You are viewing the LEDEquipment.vue file
         </div>
-    </div>
+        <div class="flex flex-col w-[11%]">
+            <a class="bg-white text-main border border-main rounded-e-3xl p-2 pl-6  my-3 font-bold border-l-0"
+                href="/oxyequipment">OXYGEN</a>
+            <a class="bg-main text-white rounded-e-3xl p-2 my-3 pl-6 font-bold" href="/ledequipment">LED</a>
+            <a class="bg-white text-main border border-main rounded-e-3xl p-2 pl-6  my-3 font-bold border-l-0"
+                href="/microequipment">MICRO</a>
+        </div>
+    </body>
 </template>
 
 <script>
--- src/views/MicroEquipment.vue
+++ src/views/MicroEquipment.vue
@@ -1,13 +1,19 @@
 <template>
-    <div>
-        You are viewing the MicroEquipment.vue file
 
-        <div class="">
-            <a href="/oxyequipment">OxygenEquipment</a>
-            <a href="/ledequipment">LedEquipment</a>
-            <a href="/microequipment">MicroEquipment</a>
+    <body class="font-futura">
+
+
+        <div>
+            You are viewing the MicroEquipment.vue file
         </div>
-    </div>
+        <div class="flex flex-col w-[11%]">
+            <a class="bg-white text-main border border-main rounded-e-3xl p-2 pl-6  my-3 font-bold border-l-0"
+                href="/oxyequipment">OXYGEN</a>
+            <a class="bg-white text-main border border-main rounded-e-3xl p-2 pl-6  my-3 font-bold border-l-0"
+                href="/ledequipment">LED</a>
+            <a class="bg-main text-white rounded-e-3xl p-2 my-3 pl-6 font-bold" href="/microequipment">MICRO</a>
+        </div>
+    </body>
 </template>
 
 <script>
