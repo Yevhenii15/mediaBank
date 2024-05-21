6a632e3fcbb80e2bdfde2d9062793a8bf20b5caf
Yevhenii <yevmot01@easv365.dk>
2024-05-21 13:22:05 +0200

    hosting

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust the limit as per your need
+  },
   plugins: [
     vue(),
   ],
