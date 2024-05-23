91eb325ec434bb3a39c60b2a1dbddef5fd21cf37
Oleh <118124646+Olehq312@users.noreply.github.com>
2024-05-23 14:43:00 +0200

    log in page mobile design

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,28 +1,29 @@
 <template>
-    <div class="w-[100%] h-[100vh] flex justify-center items-center">
-        <div class="px-3 py-10 big-border flex flex-col font-futura items-center w-[50%] rounded-3xl shadow-2xl">
-            <img class="w-[130px] drop-shadow-md" src="../images/dermaLogo.png" alt="">
-            <h1 class="text-text mt-2 mb-5 drop-shadow-md">Sign in to continue to DermaOXY's media bank</h1>
+    <div class="w-full h-screen flex justify-center items-center px-4">
+        <div class="w-full max-w-lg px-6 py-10 bg-white rounded-3xl shadow-2xl flex flex-col font-futura items-center">
+            <img class="w-32 drop-shadow-md" src="../images/dermaLogo.png" alt="DermaOXY Logo">
+            <h1 class="text-text mt-4 mb-6 text-center drop-shadow-md">Sign in to continue to DermaOXY's media bank</h1>
 
             <!-- Error message display -->
-            <div v-if="errorMessage" class="w-[65%] mb-4 text-red-500 bg-red-100 border border-red-400 rounded-xl p-2">
+            <div v-if="errorMessage"
+                class="w-full max-w-md mb-4 text-red-500 bg-red-100 border border-red-400 rounded-xl p-3">
                 {{ errorMessage }}
             </div>
 
-            <div class="username w-[65%] px-3 flex border border-border rounded-2xl">
-                <img class="w-[7%] pr-2 border-r border-border py-2" src="../images/user.svg" alt="">
-                <input class="w-[93%] pl-2" placeholder="Username" v-model="email" />
+            <div class="w-full max-w-md mb-4 px-3 flex border border-border rounded-2xl items-center">
+                <img class="w-5 pr-2 border-r border-border py-2" src="../images/user.svg" alt="User Icon">
+                <input class="w-full pl-3 py-2" placeholder="Username" v-model="email" />
             </div>
-            <div class="password my-4 w-[65%] px-3 flex border border-border rounded-2xl">
-                <img class="w-[7%] pr-2 border-r border-border py-2" src="../images/lock.svg" alt="">
-                <input class="w-[93%] pl-2" placeholder="Password" type="password" v-model="password" />
+            <div class="w-full max-w-md mb-4 px-3 flex border border-border rounded-2xl items-center">
+                <img class="w-5 pr-2 border-r border-border py-2" src="../images/lock.svg" alt="Lock Icon">
+                <input class="w-full pl-3 py-2" placeholder="Password" type="password" v-model="password" />
             </div>
-            <label class="checkbox-container w-[65%] flex justify-start">
-                <input class="w-[20px]" type="checkbox">
+            <label class="w-full max-w-md flex items-center mb-6">
+                <input class="w-4 h-4" type="checkbox">
                 <h1 class="text-text ml-3">Remember me?</h1>
             </label>
-            <button class="drop-shadow-md bg-main text-white py-2 px-10 rounded-xl font-bold mt-8" @click="logIn">LOG
-                IN</button>
+            <button class="w-full max-w-md bg-main text-white py-3 rounded-xl font-bold drop-shadow-md"
+                @click="logIn">LOG IN</button>
         </div>
     </div>
 </template>
