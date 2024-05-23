8e0ba15c171206e1a531f4872205cf97c235f2a3
Oleh <118124646+Olehq312@users.noreply.github.com>
2024-05-23 16:00:38 +0200

    app users responsive done

--- src/views/AppUsersView.vue
+++ src/views/AppUsersView.vue
@@ -1,60 +1,60 @@
 <template>
-
-  <body class="font-futura flex flex-col items-center w-[100%] py-5 relative top-[15vh] mb-[15vh]">
-    <div class="h1text flex justify-start w-[70%]">
-      <h1 class="text-[25px] text-main">Add New User:</h1>
+  <div class="px-5 lg:px-0 font-futura flex flex-col items-center w-full py-5 relative top-[15vh] mb-[15vh]">
+    <div class="h1text flex justify-start w-full md:w-3/4 px-4 md:px-0">
+      <h1 class="text-2xl text-main">Add New User:</h1>
     </div>
     <!-- Error message display -->
-    <div v-if="errorMessage" class="w-[70%] mb-4 text-red-500 bg-red-100 border border-red-400 rounded-xl p-2">
+    <div v-if="errorMessage"
+      class="w-full md:w-3/4 px-4 md:px-4 lg:px-3 mb-4 text-red-500 bg-red-100 border border-red-400 rounded-xl p-2">
       {{ errorMessage }}
     </div>
-    <div class="info flex justify-between w-[70%] mt-3 mb-5">
-      <div class="emailcl w-[45%]">
+    <div class="info flex flex-col md:flex-row justify-between w-full md:w-3/4 px-4 md:px-0 mt-3 mb-5">
+      <div class="emailcl w-full md:w-2/5 mb-4 md:mb-0">
         <h1 class="text-text">Email</h1>
-        <input class="border rounded-lg border-text w-[100%]" v-model="email" />
+        <input class="border rounded-lg border-text w-full p-2" v-model="email" />
       </div>
-      <div class="passwordcl w-[45%]">
+      <div class="passwordcl w-full md:w-2/5">
         <h1>Password</h1>
-        <input class="border rounded-lg border-text w-[100%]" type="password" v-model="password" />
+        <input class="border rounded-lg border-text w-full p-2" type="password" v-model="password" />
       </div>
     </div>
-    <div class="flex justify-between w-[70%]">
-      <div class="w-[20%] flex justify-between text-h3">
-        <label>
-          <input type="radio" value="user" v-model="selectedRole" /> User
+    <div class="flex flex-col md:flex-row justify-between w-full md:w-3/4 px-4 md:px-0">
+      <div class="w-full md:w-1/4 flex justify-between mb-4 md:mb-0 text-lg">
+        <label class="flex items-center">
+          <input type="radio" value="user" v-model="selectedRole" class="mr-2" /> User
         </label>
-        <label>
-          <input type="radio" value="admin" v-model="selectedRole" /> Admin
+        <label class="flex items-center">
+          <input type="radio" value="admin" v-model="selectedRole" class="mr-2" /> Admin
         </label>
       </div>
-      <button class="bg-main text-white px-[60px] rounded-2xl" @click="handleSignUp">Save</button>
+      <button class="bg-main text-white px-6 py-2 rounded-2xl" @click="handleSignUp">Save</button>
     </div>
 
     <!-- Display users -->
-    <div class="w-[70%] mt-5">
-      <h2 class="text-[25px] text-main mb-4">All Users:</h2>
+    <div class="w-full md:w-3/4 px-4 md:px-0 mt-5">
+      <h2 class="text-2xl text-main mb-4">All Users:</h2>
       <div class="flex flex-col bg-white">
         <div class="flex py-2 border-b border-main font-bold justify-between">
-          <div class="w-1/3 pl-4">Email</div> <!-- Adjusted alignment -->
-          <div class="w-1/3 flex">
-            <div class="w-1/2 justify-end flex pr-6">User</div> <!-- Justify between -->
-            <div class="w-1/2 justify-end flex pr-5">Admin</div> <!-- Justify end -->
+          <div class="w-1/3 pl-4">Email</div>
+          <div class="w-1/3 flex justify-end pr-4">
+            <div class="w-1/2 flex justify-end">User</div>
+            <div class="w-1/2 flex justify-end">Admin</div>
           </div>
         </div>
         <div v-for="user in users" :key="user.email" class="flex justify-between py-2 border-b">
-          <div class="w-1/3 pl-4">{{ user.email }}</div> <!-- Adjusted alignment -->
-          <div class="flex w-1/3 ">
-            <div class="w-1/2 flex justify-end pr-7 ">
+          <div class="w-1/3 pl-4">{{ user.email }}</div>
+          <div class="w-1/3 flex justify-end pr-4">
+            <div class="w-1/2 flex justify-end">
               <input class="checkbox-blue" type="checkbox" disabled :checked="user.role === 'user'" />
             </div>
-            <div class="w-1/2 flex justify-end pr-8">
+            <div class="w-1/2 flex justify-end">
               <input class="checkbox-blue" type="checkbox" disabled :checked="user.role === 'admin'" />
             </div>
           </div>
         </div>
       </div>
     </div>
-  </body>
+  </div>
 </template>
 
 <script setup>
@@ -104,7 +104,6 @@ onMounted(() => {
 
 .checkbox-blue:checked {
   background-color: #5d89b3;
-  /* DodgerBlue color */
   border-color: #5d89b3;
 }
 
